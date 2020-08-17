540937be302701024e00fb1d01fd3957b8c93c40
Chris Johnson <cxjohnson@gmail.com>
2020-08-16 22:58:37 -0500

    Add PHPStan config.
    Add PHPStan static code analysis tool configuration, level 6.
    Current code passes.

---
+++ LogoListTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file LogoListTest.php
+ * Replace with one line description.
+ */
+
+use Cxj\LogoList;
+use PHPUnit\Framework\TestCase;
+
+
+class LogoListTest extends TestCase
+{
+
+}
